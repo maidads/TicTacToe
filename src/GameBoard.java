d920a2e5227cb88b1efeab1b35ab1ba459b0ed89
Maida <maidasahiti@MacBook-Air-som-tillhor-Maida.local>
2023-10-12 00:36:32 +0200

    Jag skapade en funktion som heter createBoard för att skapa brädet. Jag skapade en tvådimensionell array med 3 rader och 3 kolumner. Jag skapade två loopar. En loop inuti en annan loop. Den går igenom varje plats i min array. Jag konverterar resultatet till ett tecken. Så att jag kan få tecken 1 till 9 och inte riktiga siffror..

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,3 +1,16 @@
 public class GameBoard {
-    
+    public static char[][] createBoard() {
+        char[][] board = new char[3][3];        // two-dimensional array with 3 rows and 3 columns
+        int count = 1;
+        for (int i = 0; i < 3; i++) {           // These two loops go through every place in my 3x3 array
+            for (int j = 0; j < 3; j++) {
+                // I assign each location in my array a char. I convert the result to a character.
+                board[i][j] = (char) (count + '0');
+                count++;
+            }
+        }
+        return board;
+    }
+
+
 }
