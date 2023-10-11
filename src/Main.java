fd9bb2bc832e082cc9efb7da81507e27ca33acc6
Maida <maidasahiti@MacBook-Air-som-tillhor-Maida.local>
2023-10-12 00:41:02 +0200

    Jag ropar funktionen createboard i main så att brädet skapasoch fylls i med siffrorna 1 till 9 som karaktärer.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main extends GameBoard {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to the game called TicTacToe!!");
+        char[][] board = createBoard();     // creates and fills the board
     }
 }
