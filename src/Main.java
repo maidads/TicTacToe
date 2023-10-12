8ba330c705e46ccbc0d2cc4475270d7895183ecc
Maida <maidasahiti@MacBook-Air-som-tillhor-Maida.local>
2023-10-12 14:33:48 +0200

    Jag gjorde en currentPlayer där jag skrev att den är X först. Jag gjorde en for loop för att gå genom alla 9 drag. Det går inte mer än 9 drag för det finns bara 9 platser i mitt spel. For loopen börjar med be spelaren X välja en plats.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class Main extends GameBoard {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Welcome to the game called TicTacToe!!");
+
         char[][] board = createBoard();     // creates and fills the board
         printBoard(board);                  // prints it
+
+        int moveCount = 0;
+        char currentPlayer = 'X';
+
+        for (int i = 0; i < 9; i++) {
+            System.out.println("Player " + currentPlayer + ", make your move (1-9):");
+            int playerMove = sc.nextInt();
+            sc.nextLine();
+        }
+
     }
 }
