a2caa73f3e958a833ab1e628082f282de26dd9b8
Maida <maidasahiti@MacBook-Air-som-tillhor-Maida.local>
2023-10-12 17:26:50 +0200

    Jag gjorde en do-while loop för att fråga spelare om dom vill spela igen och om han svarar 'Yes' så börjar spelet om. For loopen avslutas nu när någon har vunnit eftersom jag använde break startAgain;

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,40 @@ public class Main extends GameBoard {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the game called TicTacToe!!");
-        char[][] board = createBoard();     // creates and fills the board
-        printBoard(board);                  // prints it
+        String playAgain;
 
-        int moveCount = 0;
-        char currentPlayer = 'X';
+        do {
+            char[][] board = createBoard();     // creates and fills the board
+            printBoard(board);                  // prints it
 
-        for (int i = 0; i < 9; i++) {
-            boolean moveMade = false;
-            while (!moveMade) {
-                System.out.println("Player " + currentPlayer + ", choose a place 1-9: ");
-                int move = getPlayerMove(sc);
-                sc.nextLine();
-                moveMade = Move.makeMove(board, move, currentPlayer);
-                printBoard(board);
-                moveCount++;            // ++ för every move
+            int moveCount = 0;
+            char currentPlayer = 'X';
 
-                boolean gameOver = CheckWin.checkIfWin(board);
-                if (gameOver) {
-                    System.out.println("Player " + currentPlayer + " wins!");
-                    break;
-                } else if (moveCount == 9) {
-                    System.out.println("It's a draw!");
-                    break;
+            startAgain:
+            for (int i = 0; i < 9; i++) {
+                boolean moveMade = false;
+                while (!moveMade) {
+                    System.out.println("Player " + currentPlayer + ", choose a place 1-9: ");
+                    int move = getPlayerMove(sc);
+                    sc.nextLine();
+                    moveMade = Move.makeMove(board, move, currentPlayer);
+                    printBoard(board);
+                    moveCount++;            // ++ för every move
+
+                    boolean gameOver = CheckWin.checkIfWin(board);
+                    if (gameOver) {
+                        System.out.println("Player " + currentPlayer + " wins!");
+                        break startAgain;
+                    } else if (moveCount == 9) {
+                        System.out.println("It's a draw!");
+                        break startAgain;
+                    }
                 }
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
             }
-            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-        }
-        System.out.println("Thank you for playing my game! ");
-        System.out.println("Do you want to play again? Type 'Yes' or 'No': ");
+            System.out.println("Thank you for playing my game! ");
+            System.out.println("Do you want to play again? Type 'Yes' or 'No': ");
+            playAgain = sc.nextLine();
+        } while (playAgain.equalsIgnoreCase("Yes"));
     }
 }
