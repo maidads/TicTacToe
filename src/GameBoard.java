df3e8c21c59cbd3554cdd5a9ecdb2ea1515d392d
Maida <maidasahiti@MacBook-Air-som-tillhor-Maida.local>
2023-10-13 10:00:55 +0200

    Ändrat kommentarer till engelska.

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -2,9 +2,9 @@ public class GameBoard extends Move {
     public static char[][] createBoard() {
         char[][] board = new char[3][3];        // two-dimensional array with 3 rows and 3 columns
         int count = 1;
-        for (int i = 0; i < 3; i++) {           // These two loops go through every place in my 3x3 array
+        for (int i = 0; i < 3; i++) {           // these two loops go through every place in my 3x3 array
             for (int j = 0; j < 3; j++) {
-                // I assign each location in my array a char. I convert the result to a character.
+                // i assign each location in my array a char. I convert the result to a character.
                 board[i][j] = (char) (count + '0');
                 count++;
             }
@@ -18,9 +18,9 @@ public class GameBoard extends Move {
             for (int j = 0; j < 3; j++) {
                 System.out.print("| " + board[i][j] + " ");
             }
-            System.out.println("|"); // Lägger till den sista vertikala linjen och går till en ny rad
-            if (i < 2) { // Om vi inte är på den sista raden
-                System.out.println("-------------"); // Skriver ut en rad med horisontella linjer
+            System.out.println("|");        // I put the last line
+            if (i < 2) {                    // if we are not on the last row
+                System.out.println("-------------");        // prints horizontal lines
             }
         }
     }
